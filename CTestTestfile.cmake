a1c4fb28888715aa2e8e8468ea29b6ceff3f36de
Daniel Fagnan <dnfagnan@gmail.com>
2014-02-22 23:57:50 -0700

    removed cmake junk
    Signed-off-by: Daniel Fagnan <dnfagnan@gmail.com>

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/daniel/Projects/gc
-# Build directory: /Users/daniel/Projects/gc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(testgc "testgc")
