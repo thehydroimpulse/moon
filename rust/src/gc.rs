de558a57211e504d6c566ad4ba77aa61a6af1f7a
Daniel Fagnan <dnfagnan@gmail.com>
2014-03-15 01:04:58 -0600

    added new roots test
    Signed-off-by: Daniel Fagnan <dnfagnan@gmail.com>

--- rust/src/gc.rs
+++ rust/src/gc.rs
@@ -4,7 +4,7 @@ use std::rc::Rc;
 #[experimental]
 pub struct Gc {
   /// Describe the GC roots that we have. These are stack values.
-  roots: ~[Value]
+  roots: ~[Rc<Value>]
 }
 
 #[experimental]
@@ -16,7 +16,7 @@ impl Gc {
   }
 
   pub fn register_root(&mut self, root: Rc<Value>) {
-
+    self.roots.push(root);
   }
 }
 
@@ -24,11 +24,21 @@ impl Gc {
 #[cfg(test)]
 mod test {
   use super::Gc;
+  use vm::Value;
+  use vm::Int;
+  use std::rc::Rc;
 
   #[test]
   fn should_have_empty_roots() {
     let gc = Gc::new();
     assert_eq!(gc.roots.len(), 0);
+  }
+
+  #[test]
+  fn should_add_root() {
+    let val = Int(55);
+    let mut gc = Gc::new();
+    gc.register_root(Rc::new(val));
   }
 
 }
