b93bd30a8454906373ea11241a354b80a813ed2a
Daniel Fagnan <dnfagnan@gmail.com>
2014-03-16 04:48:49 -0600

    more tests
    Signed-off-by: Daniel Fagnan <dnfagnan@gmail.com>

--- src/gen.rs
+++ src/gen.rs
@@ -12,3 +12,23 @@ pub fn gen_add(a: &Ast, b: &Ast) -> OpCode {
         _ => fail!("[gen_add] expected an integer.")
     }
 }
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use ast::{NumberExprAst};
+    use vm::{XAdd};
+
+    #[test]
+    fn test_gen_add() {
+        let op = gen_add(&NumberExprAst(5), &NumberExprAst(2));
+        match op {
+            XAdd(a, b) => {
+                assert_eq!(a, 5);
+                assert_eq!(b, 2);
+            },
+            _ => fail!("Failed")
+        }
+    }
+}
