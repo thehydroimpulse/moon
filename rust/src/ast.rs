a62422559f43cb3c2627d7fde057785669bab9d2
Daniel Fagnan <dnfagnan@gmail.com>
2014-03-15 20:34:26 -0600

    new ast system
    Signed-off-by: Daniel Fagnan <dnfagnan@gmail.com>

--- rust/src/ast.rs
+++ rust/src/ast.rs
@@ -1,62 +1,15 @@
 use std::rc::Rc;
 use lexer;
 
-pub trait Ast {}
-
-
-pub struct VariableExpr {
-    name: ~str
+pub enum Ast {
+    VariableExprAst(~str),
+    NumberExprAst(i32),
+    BinaryExprAst(lexer::Token, ~Ast, ~Ast)
 }
 
-impl Ast for VariableExpr {}
-impl VariableExpr {
-    pub fn new(name: ~str) -> VariableExpr {
-        VariableExpr {
-            name: name
-        }
-    }
-}
-
-
-pub struct NumberExpr {
-    value: i32
-}
-
-impl Ast for NumberExpr {}
-impl NumberExpr {
-    pub fn new(value: i32) -> NumberExpr {
-        NumberExpr {
-            value: value
-        }
-    }
-}
-
-pub struct BinaryExpr {
-    op: lexer::Token,
-    lhs: ~Ast,
-    rhs: ~Ast
-}
-
-impl Ast for BinaryExpr {}
-impl BinaryExpr {
-    pub fn new(op: lexer::Token, lhs: ~Ast, rhs: ~Ast) -> BinaryExpr {
-        BinaryExpr {
-            op: op,
-            lhs: lhs,
-            rhs: rhs
-        }
-    }
-}
 
 #[cfg(test)]
 mod test {
     use super::Ast;
-    use super::VariableExpr;
-
-    #[test]
-    fn should_create_new_var() {
-        let var = VariableExpr::new(~"hello");
-        assert_eq!(var.name, ~"hello");
-    }
-
+ 
 }
--- rust/src/parser.rs
+++ rust/src/parser.rs
@@ -1,5 +1,7 @@
 use lexer::TokenValue;
 use lexer;
+use ast::{Ast, NumberExprAst, VariableExprAst, BinaryExprAst};
+use std::from_str;
 
 pub struct Parser<'a> {
     current_token: Option<TokenValue>,
@@ -12,5 +14,20 @@ impl<'a> Parser<'a> {
             current_token: None,
             lexer: lexer::Lexer::new(input)
         }
+    }
+
+    pub fn bump(&mut self) {
+        self.current_token = self.lexer.next_token();
+    }
+
+    pub fn parse(&mut self) {
+
+    }
+
+    pub fn parse_number(&mut self) -> Ast {
+        let num: i32 = from_str(self.current_token.unwrap().value).unwrap();
+        let expr = NumberExprAst(num);
+        self.bump();
+        return expr;
     }
 }
