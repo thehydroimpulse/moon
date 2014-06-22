dc19b937f4586f529dd5b1b8367441bd898c8759
Daniel Fagnan <dnfagnan@gmail.com>
2014-06-21 22:00:15 -0600

    Working on the bump method

--- src/moon/lexer.rs
+++ src/moon/lexer.rs
@@ -11,7 +11,8 @@ pub enum Token {
     CARET,
     LPAREN,
     RPAREN,
-    Noop
+    Noop,
+    Done
 }
 
 /// The lexer is the system that has any notion of an input stream. The
@@ -42,14 +43,42 @@ impl<'a> Lexer<'a> {
     /// This doesn't return the token, it stores it
     /// in the lexer.
     pub fn bump(&mut self) {
+        // Fetch the next char.
+        let c = match self.iter.next() {
+            Some(c) => c,
+            None => {
+                self.token = Done;
+                return;
+            }
+        };
 
+        // Begin parsing an s-expression.
+        if c == '(' {
+
+        }
     }
 
+    /// Expect a given token to come next, otherwise
+    /// fail. Failing will result in a formatted
+    /// error message that uses the Span and lets
+    /// the user know what went wrong.
     pub fn expect(&mut self, tok: &Token) {
         if self.token == *tok {
             self.bump();
         } else {
             fail!("Expected {} but found {}", *tok, self.token);
+        }
+    }
+
+    /// Optionally eat a given token. If the token is not
+    /// found, this will simply return false and
+    /// won't fail.
+    pub fn eat(&mut self, tok: &Token) -> bool {
+        if self.token == *tok {
+            self.bump();
+            true
+        } else {
+            false
         }
     }
 }
