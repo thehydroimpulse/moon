a706f4ecac3cb6a5ea9bcd6198e0bfb23f5b9e34
Daniel Fagnan <dnfagnan@gmail.com>
2014-06-21 21:46:52 -0600

    Rough design for the lexer methods

--- src/moon/lexer.rs
+++ src/moon/lexer.rs
@@ -1,6 +1,18 @@
 use std::str::Chars;
 use std::io::Reader;
 
+#[deriving(PartialEq, Show)]
+pub enum Token {
+    PLUS,
+    MINUS,
+    TIMES,
+    AT,
+    CARET,
+    LPAREN,
+    RPAREN,
+    Noop
+}
+
 /// The lexer is the system that has any notion of an input stream. The
 /// stream can come from many sources, thus, we use the Reader trait
 /// that is implemented for strings, files, tcp, etc...
@@ -9,14 +21,32 @@ use std::io::Reader;
 /// that will be a bottleneck for larger input sources.
 pub struct Lexer<'a> {
     source: &'a str,
-    iter: Chars<'a>
+    iter: Chars<'a>,
+    token: Token
 }
 
 impl<'a> Lexer<'a> {
     pub fn new(source: &'a str) -> Lexer<'a> {
         Lexer {
             source: source,
-            iter: source.chars()
+            iter: source.chars(),
+            token: Noop
+        }
+    }
+
+
+    /// Parse and fetch the next token in the text stream.
+    /// This doesn't return the token, it stores it
+    /// in the lexer.
+    pub fn bump(&mut self) {
+
+    }
+
+    pub fn expect(&mut self, tok: &Token) {
+        if self.token == *tok {
+            self.bump();
+        } else {
+            fail!("Expected {} but found {}", *tok, self.token);
         }
     }
 }
